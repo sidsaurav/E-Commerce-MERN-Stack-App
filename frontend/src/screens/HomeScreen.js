f981628d2f25695389791f1ce3e2519a77623a6c
Siddharth Saurav <sid.saurav11@gmail.com>
2022-02-21 16:46:00 +0530

    implement global state using redux

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,29 +1,35 @@
-import { React, useState, useEffect } from 'react'
+import { React, useEffect } from 'react'
 import Product from '../components/Product'
+import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
-import axios from 'axios'
-
+import { listProducts } from '../actions/productActions'
 const HomeScreen = () => {
-    const [products, setProducts] = useState([])
+    const dispatch = useDispatch()
 
+    const productList = useSelector((state) => state.productList)
+    const { loading, error, products } = productList
+
+    console.log(products)
     useEffect(() => {
-        const url = '/api/products'
-        axios.get(url).then((d) => {
-            console.log(d)
-            setProducts(d.data)
-        })
-        console.log('hello')
-    }, [])
+        dispatch(listProducts())
+    }, [dispatch])
+
     return (
         <>
             <h1>Latest Products</h1>
-            <Row>
-                {products.map((p) => (
-                    <Col key={p._id} sm={6} md={4} lg={3}>
-                        <Product product={p} />
-                    </Col>
-                ))}
-            </Row>
+            {loading ? (
+                <h2>Loading...</h2>
+            ) : error ? (
+                <h2>{error}</h2>
+            ) : (
+                <Row>
+                    {products.map((p) => (
+                        <Col key={p._id} sm={6} md={4} lg={3}>
+                            <Product product={p} />
+                        </Col>
+                    ))}
+                </Row>
+            )}
         </>
     )
 }
